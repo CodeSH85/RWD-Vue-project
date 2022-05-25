6ceaefaf407f065ba3d05034a356d368aa08ee33
CodeSH85 <codesh85@gmail.com>
2022-05-26 01:32:11 +0800

    修正設定檔和路由以解決打包空白問題

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 //=============================================================
 
@@ -31,7 +32,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
